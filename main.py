bc77258808a14508c5f10cb0b565fb3b7f2d90b8
KimJongKwang <kjk6646@naver.com>
2021-12-21 23:39:39 +0900

    GET POST 테스트 추가

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from typing import Optional
 
 app = FastAPI()
 
+
 class NowTime(BaseModel):
     nowtime: Optional[str] = None
+
 
 @app.post("/nowTime")
 async def return_now_time(request: NowTime):
     return {"nowtime": request.nowtime}
 
+
 @app.get("/nowTime/date")
 async def return_now_time(time: str):
     return {
         'keyword': time,
-        'nowtime': datetime.now().strftime('%Y-%m-%d %H:%M'),
-        }
+        'nowtime': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    }

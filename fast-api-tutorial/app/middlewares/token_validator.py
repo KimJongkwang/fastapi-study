5d72a9ba8836336d5ef7356ab313ce4455b7c156
KimJongKwang <kjk6646@naver.com>
2022-03-05 23:29:58 +0900

    feat: token validator 업데이트

--- fast-api-tutorial/app/middlewares/token_validator.py
+++ fast-api-tutorial/app/middlewares/token_validator.py
@@ -2,11 +2,16 @@
 # API가 아니라 템플릿에서 렌더링이 요구되는 상황에서는 쿠키의 토큰 검사
 import time
 import typing
+import re
+import jwt
 
+from fastapi.responses import JSONResponse
+from pydantic import Json
 from starlette.requests import Request
 from starlette.datastructures import Headers
 from starlette.types import ASGIApp, Receive, Scope, Send
 
+from common import consts
 from utils.date_utils import D
 
 
@@ -30,16 +35,69 @@ class AccessControl:
         request = Request(scope=scope)
         headers = Headers(scope=scope)
 
-        request.state.jongkwang = "Made by Jongkwang Kim"
-        request.state.req_time = D.datetime()
-        print(D.datetime())
-        print(D.date())
-        print(D.date_num())
+        # request.state.jongkwang = "Made by Jongkwang Kim"
+        # None 을 넣지 않으면 호출시에 인풋 에러 발생
         request.state.start = time.time()
         request.state.inspect = None  # 500 error 시 로깅하기위해 사용함 --> SENTRY 사용함
         request.state.user = None  # Token decode 하고 user 데이터를 넣음  --> JWT를 사용하는 이유: user 인증상태를 endpoint 마다 DB를 조회하여 검사하는 것을 지양
         request.state.is_admin_access = None
+        ip_from = request.headers["x-forwarded-for"] if "x-forwarded-for" in request.headers.keys() else None
+        # x-forwarded-for << aws에서는 헤더에 ip가 위치함
+
+        if await self.url_pattern_check(request.url.path, self.except_path_regex) or request.url.path in self.except_path_list:
+            return await self.app(scope, receive, send)  # 이게 뭔가!?
+
+        if request.url.path.startswith("/api"):
+            # api 인경우 헤더로 토큰 검사
+            if "Authorization" in request.headers.keys():
+                request.state.user = await self.token_decode(access_token=request.headers.get("Authorization"))
+                # 토큰 없음
+            else:
+                if "Authorization" not in request.headers.keys():
+                    response = JSONResponse(status_code=401, content=dict(msg="Authorization Required"))
+                    return await response(scope, receive, send)
+
+        else:
+            print(request.cookies)
+            if "Authorization" not in request.cookies.keys():
+                response = JSONResponse(status_code=401, content=dict(msg="Authorization Required"))
+                return await response(scope, receive, send)
+
+            request.state.user = await self.token_decode(access_token=request.cookies.get("Authorization"))
+
+        request.state.req_time = D.datetime()
+        print(D.dateitme())
+        print(D.date())
+        print(D.date_num())
+
         print(request.cookies, "this is cookies")
         print(headers)
         res = await self.app(scope, receive, send)
         return res
+
+    @staticmethod
+    async def url_pattern_check(path, pattern):
+        """
+        토큰 검사 예외 url 체크
+
+        Args:
+            path (_type_): _description_
+            pattern (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        result = re.match(pattern, path)
+        if result:
+            return True
+        return False
+
+    @staticmethod
+    async def token_decode(access_token):
+        try:
+            access_token = access_token.replace("Bearer ", "")
+            payload = jwt.decode(access_token, key=consts.JWT_SECRET, algorithms=[consts.JWT_ALGORITHM])
+        except jwt.PyJWKError as e:
+            print(e)
+
+        return payload

acc912b0532c63be11d94abb2ce0b8ae62ea9d20
KimJongKwang <kjk6646@naver.com>
2022-03-11 00:32:28 +0900

    feat:  ApiKeys스키마 생성, CRUD 업데이트 및 테스트

--- fast-api-tutorial/app/database/schema.py
+++ fast-api-tutorial/app/database/schema.py
@@ -1,6 +1,5 @@
-
 from sqlalchemy.schema import Column
-from sqlalchemy import Integer, DateTime, func, Enum, String, Boolean
+from sqlalchemy import ForeignKey, Integer, DateTime, func, Enum, String, Boolean
 from sqlalchemy.orm import Session
 from database.conn import db
 
@@ -21,7 +20,6 @@ class BaseMixin:
         """
         self._q = None
         self._session = None
-        self.served = None
 
     def all_columns(self):
         return [c for c in self.__table__.columns if c.primary_key is False and c.name != "created_at"]
@@ -39,7 +37,7 @@ class BaseMixin:
             kwagrs: 적재할 데이터들
         """
         obj = cls()
-        print(obj._q)  # 여기서 _q를 받아 실행시키더라도 None 이 아니라 해당 어트리뷰트가 없음이 반환됨. __init__이 실행되지 않는 것 같음
+        # print(obj._q)  # 여기서 _q를 받아 실행시키더라도 None 이 아니라 해당 어트리뷰트가 없음이 반환됨. __init__이 실행되지 않는 것 같음
         for col in obj.all_columns():
             col_name = col.name
             if col_name in kwagrs:
@@ -65,7 +63,7 @@ class BaseMixin:
         # get(select)은 commit이 필요 없기 때문에, 추가로 세션을 하나 더 받아온다. 이때 next를 사용하여 return 시 자동으로 세션이 종료되게끔 유도
         session = next(db.session())  # 단, 트래픽이 얼마나 발생할지 여부에 따라 next()로 받는 것보다, parameter로 session을 받아 오는것이 좋을 수 있음
         query = session.query(cls)
-        print(query)
+        print(query, "@@@@@@@@@@@@@@@ 이건 get 쿼리")
         for key, val in kwagrs.items():
             col = getattr(cls, key)
             query = query.filter(col == val)  # filter 정의: 2개 이상 로우를 전달
@@ -95,23 +93,37 @@ class BaseMixin:
             if len(key) > 2:
                 raise Exception("No 2 more dunders")
             col = getattr(cls, key[0])
-            if len(key) == 1: cond.append((col == val))
-            elif len(key) == 2 and key[1] == "gt": cond.append((col > val))
-            elif len(key) == 2 and key[1] == "gte": cond.append((col >= val))
-            elif len(key) == 2 and key[1] == "lt": cond.append((col < val))
-            elif len(key) == 2 and key[1] == "lte": cond.append((col <= val))
-            elif len(key) == 2 and key[1] == "in": cond.append((col.in_(val)))
+            if len(key) == 1:
+                cond.append((col == val))
+            elif len(key) == 2 and key[1] == "gt":
+                cond.append((col > val))
+            elif len(key) == 2 and key[1] == "gte":
+                cond.append((col >= val))
+            elif len(key) == 2 and key[1] == "lt":
+                cond.append((col < val))
+            elif len(key) == 2 and key[1] == "lte":
+                cond.append((col <= val))
+            elif len(key) == 2 and key[1] == "in":
+                cond.append((col.in_(val)))
         obj = cls()
         if session:
             obj._session = session
-            obj.served = True
+            obj._sess_served = True
         else:
             obj._session = next(db.session())
-            obj.served = False
+            obj._sess_served = False
         query = obj._session.query(cls)
         query = query.filter(*cond)
         obj._q = query
         return obj
+
+    @classmethod
+    def cls_attr(cls, col_name=None):
+        if col_name:
+            col = getattr(cls, col_name)
+            return col
+        else:
+            return cls
 
     def order_by(self, *args: str):
         for a in args:
@@ -125,6 +137,41 @@ class BaseMixin:
             col = self.cls_attr(col_name)  # ???? 뭐지
             self._q = self._q.order_by(col.asc()) if is_asc else self._q.order_by(col.desc())  # 이건 또 뭐지 sqlalchemy 함수인듯
         return self
+
+    def update(self, auto_commit: bool = False, **kwagrs):
+        result = self._q.update(kwagrs)
+        self._session.flush()
+        if auto_commit:
+            self._session.commit()
+        return result  # 요건 좀 더 확인해보자. update인데, result를 반환?
+
+    def first(self):
+        result = self._q.first()
+        self.close()
+        return result
+
+    def delete(self, auto_commit: bool = False, **kwagrs):
+        self._q.delete()
+        if auto_commit:
+            self._session.commit()
+        # 삭제가 되면 끝이기 때문에 리턴하지 않음
+
+    def all(self):
+        result = self._q.all()
+        self.close()
+        return result
+
+    def count(self):
+        result = self._q.count()
+        self.close()
+        return result
+
+    def close(self):
+        if self._sess_served:
+            self._session.commit()
+            self._session.close()
+        else:
+            self._session.flush()
 
 
 class Users(Base, BaseMixin):
@@ -153,3 +200,13 @@ class Users(Base, BaseMixin):
     updated_at DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP   -- 업데이트시 현재 시간
     );
     """
+
+
+class ApiKeys(Base, BaseMixin):
+    __tablename__ = "api_keys"
+    access_key = Column(String(length=64), nullable=False, index=True)
+    secret_key = Column(String(length=64), nullable=False)
+    user_memo = Column(String(length=40), nullable=True)
+    status = Column(Enum("active", "deleted", "blocked"), default="active")
+    is_whitelisted = Column(Boolean, default=False)  # api 로 받아낼 수 있는 IP주소
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
--- fast-api-tutorial/app/routes/users.py
+++ fast-api-tutorial/app/routes/users.py
@@ -1,19 +1,30 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 
 from models import UserMe
+from sqlalchemy.orm import Session
 from database.schema import Users
+from database.conn import db
 
 router = APIRouter()
 
 
 # response_model을 안 넣게되면, user.id 로 조회되는 모든 컬럼 전송
-@router.get("/me", response_model=UserMe)
+# @router.get("/me")
+# async def get_user(request: Request, session: Session = Depends(db.session)):  
+# 위의 파라미터로 db session을 받으면, 
+# filter(session)로 사용할 수 있는데, 
+# DB를 한번만 가져와 사용할 경우 굳이 여러개 세션을 사용할 필요가 없음
+# 지속적인 DB작업이 필요하다면 session을 새로 받아오는 것도 방법
+@router.get("/me")
 async def get_user(request: Request):
     """
     get my info
     """
     user = request.state.user
-    user_info = Users.get(id=user.id)
+    print(user.id)
+    # user_info = Users.get(id=user.id)
+    user_info = Users.filter(id__gt=user.id).order_by("email").count()  # 이건 장고스타일
+    # user_info = session.query(Users).filter(Users.id > user.id).order_by(Users.email.asc()).count()  # sql alchemy 스타일
     # from errors.exceptions import NotFoundUserEx
     # raise NotFoundUserEx(user_info.email) # 에러처리 테스트
     return user_info

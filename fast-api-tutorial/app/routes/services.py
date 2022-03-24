7ba65b2bd8ba4a9d2460d3e7e1180fd5253d3193
KimJongKwang <kjk6646@naver.com>
2022-03-24 23:18:37 +0900

    test: swagger에서 테스트하기 위한 디버깅 코드

--- fast-api-tutorial/app/routes/services.py
+++ fast-api-tutorial/app/routes/services.py
@@ -3,7 +3,8 @@ import json
 import requests
 
 from fastapi import Request, APIRouter
-
+from fastapi.logger import logger
+from common._key import KAKAO_KEY
 from errors import exceptions as ex
 from models import KakaoMsgBody, MessageOk
 
@@ -17,10 +18,14 @@ async def get_all_service(request: Request):
 
 @router.post("kakao/send")
 async def send_kakao(request: Request, body: KakaoMsgBody):
-    token = os.environ.get("KAKAO_KEY", "")
+    # environ: 시스템 환경변수 추가(KAKAO_KEY 추가없을 경우 _key에서 가져옴.)
+    token = os.environ.get("KAKAO_KEY", KAKAO_KEY)
     headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/x-www-form-urlencoded"}
+    if len(body.msg) >= 200:
+        raise ex.OverCharLongEx()
 
-    body = dict(object_type="text", text=body.msg, link=dict(web_url="https://naver.com", mobile_url="https://naver.com"), button_title="지금 확인")
+    # LINK 버튼이 안넘어간다. TEXT만 넘어가는데, 원인을 찾아보자.
+    body = dict(object_type="text", text=body.msg, link=dict(web_url="https://velog.io/@ceaseless", mobile_url="https://velog.io/@ceaseless"), button_title="지금 확인")
     data = {"template_object": json.dumps(body, ensure_ascii=False)}
 
     res = requests.post("https://kapi.kakao.com/v2/api/talk/memo/default/send", headers=headers, data=data)
